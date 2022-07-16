9466b65b9ac4baebe9a7467cc42284e8b736d248
Andreev Vadim <76976870+andrground-gg@users.noreply.github.com>
2022-07-16 18:45:16 +0300

    Added endgame screen and abillity to restart

--- GMTK2022Game/Assets/Scripts/CameraLerp.cs
+++ GMTK2022Game/Assets/Scripts/CameraLerp.cs
@@ -11,6 +11,9 @@ public class CameraLerp : MonoBehaviour
     private float _lerpPower;
     void FixedUpdate()
     {
+        if (_target == null)
+            return;
+
         transform.position = Vector3.Lerp(transform.position, new Vector3(_target.position.x, _target.position.y, transform.position.z), _lerpPower * Time.fixedDeltaTime);
     }
 }
