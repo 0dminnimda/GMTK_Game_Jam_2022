7b1018d67735523081abb48845ec5d4dd54f8cfc
globe <56902389+groesie@users.noreply.github.com>
2022-07-16 18:49:54 +0700

    Update Enemy.cs

--- GMTK2022Game/Assets/Scripts/Enemy.cs
+++ GMTK2022Game/Assets/Scripts/Enemy.cs
@@ -25,13 +25,13 @@ public class Enemy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         _targetPos = _target.transform.position;
 
         RotateTowardsTarget();
 
-        float step = _enemySpeed * Time.deltaTime;
+        float step = _enemySpeed * Time.fixedDeltaTime;
         transform.position = Vector2.MoveTowards(transform.position, _targetPos, step);
     }
 
@@ -40,7 +40,7 @@ public class Enemy : MonoBehaviour
         Vector2 vectorToTarget = _targetPos - transform.position;
         float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * _rotationSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.fixedDeltaTime * _rotationSpeed);
     }
 
     IEnumerator DoCheck()
