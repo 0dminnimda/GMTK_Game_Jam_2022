68cc2057474c9d1a8a411faf0681f1d349af39e8
0dminnimda <0dminnimda@gmail.com>
2022-07-16 18:18:27 +0300

    Require Health to be present for Healthbar

--- GMTK2022Game/Assets/Scripts/Healthbar.cs
+++ GMTK2022Game/Assets/Scripts/Healthbar.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 public class Healthbar : MonoBehaviour
 {
     [SerializeField]
