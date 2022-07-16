f5e83aaa2c91c41a8013c88023fb32d44191e15d
Andreev Vadim <76976870+andrground-gg@users.noreply.github.com>
2022-07-16 12:56:10 +0300

    Implement enemy following (#8)
    * made enemy follow player and rotate towards him
    
    * Apply suggestions from code review
    
    Co-authored-by: 0dminnimda <0dminnimda@gmail.com>

--- GMTK2022Game/Assets/Scripts/Enemy.cs
+++ GMTK2022Game/Assets/Scripts/Enemy.cs
@@ -5,17 +5,42 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
+    private float _enemySpeed = 0.75f;
+
+    [SerializeField]
+    private float _rotationSpeed = 2f;
+
+    [SerializeField]
     private List<Weapon> _weapons;
+
+    private GameObject _target;
+    private Vector3 _targetPos;
+
     // Start is called before the first frame update
     void Start()
     {
+        _target = FindObjectOfType<MainCharacter>().gameObject;
+
         StartCoroutine(nameof(DoCheck));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _targetPos = _target.transform.position;
+
+        RotateTowardsTarget();
+
+        float step = _enemySpeed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, _targetPos, step);
+    }
+
+    private void RotateTowardsTarget()
+    {
+        Vector2 vectorToTarget = _targetPos - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * _rotationSpeed);
     }
 
     IEnumerator DoCheck()
