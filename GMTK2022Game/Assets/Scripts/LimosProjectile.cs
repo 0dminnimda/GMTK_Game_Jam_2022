5438db6cb94e1b30a019d67b8b8db1da487560cd
Limofeus <69484438+Limofeus@users.noreply.github.com>
2022-07-17 20:13:26 +0300

    fixes

--- GMTK2022Game/Assets/Scripts/LimosProjectile.cs
+++ GMTK2022Game/Assets/Scripts/LimosProjectile.cs
@@ -8,9 +8,17 @@ public class LimosProjectile : MonoBehaviour
     public bool playerBullet;
     public float baseSpeed;
     public float missChance;
+    public float deleteIn;
+    private float timer;
+    public bool destroyOnHit;
     void Update()
     {
         transform.Translate(Vector3.up * Time.deltaTime * baseSpeed);
+        if(timer > deleteIn)
+        {
+            Destroy(this.gameObject);
+        }
+        timer += Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -18,7 +26,13 @@ public class LimosProjectile : MonoBehaviour
         if (collision.gameObject.GetComponent<Health>() != null)
         {
             if(Random.Range(0f, 1f) >= missChance)
+            {
                 collision.gameObject.GetComponent<Health>().DealDamage(damage, playerBullet ? Assets.Scripts.Enums.DamageLayer.Enemy : Assets.Scripts.Enums.DamageLayer.Player);
+                if (destroyOnHit)
+                {
+                    Destroy(gameObject);
+                }
+            }
         }
 
     }
